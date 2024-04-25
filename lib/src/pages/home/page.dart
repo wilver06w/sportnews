551eb674577ad06ae14d68f64355fb11930d9607
Wilver Fabian Rojas Del Ducca <wilverfabian.06@gmail.com>
2024-04-25 11:16:35 -0500

    Feat: Separated listNews with StreamController

--- lib/src/pages/home/page.dart
+++ lib/src/pages/home/page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:sportnews/src/pages/home/models/news_model.dart';
-import 'package:sportnews/src/pages/home/widget/card.dart';
 import 'package:sportnews/src/pages/home/widget/input_search.dart';
+import 'package:sportnews/src/pages/home/widget/list_news.dart';
 import 'package:sportnews/src/shared/constant/sport_news_ui.dart';
 
 class Page extends StatelessWidget {
@@ -34,53 +33,12 @@ class Page extends StatelessWidget {
               child: const InputSearch(),
             ),
             const Gap(12),
-            Expanded(
-              child: ListView.separated(
-                shrinkWrap: true,
-                itemCount: listSportNews.length,
-                itemBuilder: (context, index) {
-                  final item = listSportNews[index];
-                  return CardChecked(
-                    imagen: item.imageId,
-                    index: index,
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return const Gap(20);
-                },
-              ),
-            )
+            const Expanded(
+              child: ListViewNews(),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-const listSportNews =  [
-  NewsModels(
-    imageId: 'assets/img2.jpg',
-    title: 'Football players Kids',
-    id: 0,
-  ),
-  NewsModels(
-    imageId: 'assets/img1.jpg',
-    title: 'Football players Yellow',
-    id: 1,
-  ),
-  NewsModels(
-    imageId: 'assets/img3.jpg',
-    title: 'Ball control',
-    id: 2,
-  ),
-  NewsModels(
-    imageId: 'assets/img4.jpg',
-    title: 'Soccer play',
-    id: 3,
-  ),
-  NewsModels(
-    imageId: 'assets/img5.jpg',
-    title: 'Throw-in Ball',
-    id: 2,
-  ),
-];
